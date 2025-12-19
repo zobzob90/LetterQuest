fc90d1af59e9c91df1e76725a3d74dfa1628a0b4
Eric Trignau <ertrigna@f6r8s10.clusters.42paris.fr>
2025-12-19 19:16:19 +0100

    push guessedLetter function

--- src/components/WordInput.js
+++ src/components/WordInput.js
@@ -1,12 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   WordInput.jsx                                      :+:      :+:    :+:   */
+/*   WordInput.js                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 16:39:54 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/12/19 16:39:55 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/12/19 19:12:41 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+export function getHiddenWord(word, guessedLetters)
+{
+	return word.split('').map(letter => (guessedLetters.includes(letter) ? letter : "_")).join(' ');
+}
+
+export function isLetterInWord(letter, word)
+{
+	if (word.includes(letter))
+		return true;
+	else
+		return false;
+}
--- src/data/words.js
+++ src/data/words.js
@@ -6,11 +6,11 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 16:39:57 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/12/19 18:18:48 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/12/19 18:50:05 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-export const wordsData = ["chat", "chien", "jouet", "code", "lapin", "maison", "appartement", "garantie", "console", "constitution", "test", "langue", "mouton"]
+export const wordsData = ["chat", "chien", "jouet", "code", "lapin", "maison", "appartement", "garantie", "console", "constitution", "test", "langue", "mouton", "arme", "fusil"]
 
 export function getRandomWord()
 {
